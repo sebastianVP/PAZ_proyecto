e71356c3bed652c49164e711d439bae398e55fcb
sebastianVP <alex.valdezp22@gmail.com>
2025-03-10 22:59:59 -0500

    update

--- galeria/views.py
+++ galeria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def ver_imagenes(request):
+    imagenes = [
+        'galeria/f_001.jpeg',
+        'galeria/f_002.jpeg',
+        'galeria/f_003.jpeg',
+    ]
+    return render(request, 'galeria/galeria.html', {'imagenes': imagenes})
